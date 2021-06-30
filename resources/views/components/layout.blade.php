f1fba34981c4d56e38abbd5036352d6daf25223c
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-06-30 18:39:49 +0100

    Added skeleton pages

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body class="antialiased">
+<x-main-nav/>
+{{ $slot }}
+<x-footer/>
+</body>
+</html>
---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <div class="container mx-auto my-6">
+        <h1 class="font-bold text-4xl text-center">Contact us</h1>
+    </div>
+</x-layout>
